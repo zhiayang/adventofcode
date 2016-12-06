3b2e4a6aeb31cc8abd5ae911a5880bee60cedf4b
zhiayang <zhiayang@gmail.com>
2016-12-07 00:49:28 +0800

    day6

---
+++ build.fish
@@ -0,0 +1,16 @@
+#!/usr/local/bin/fish
+
+set SYSROOT "$HOME/Desktop/Stuff/Orion\ Industries/flax/build/sysroot"
+set FLAXC "$SYSROOT/usr/local/bin/flaxc"
+
+# usage:
+# build.fish $YEAR $DAY
+
+if test -z "$argv"
+	echo "Argument required"
+else
+	# echo "Compiling..."
+	eval time "$FLAXC" -sysroot $SYSROOT -O3 -run $argv[1]/"day"$argv[2]/prog.flx
+	# echo -e "Compiled.\n"
+	# eval $argv[1]/"day"$argv[2]/prog
+end
